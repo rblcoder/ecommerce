01014f3149d539b906993eb32ca3497b90fd2fb0
Rupa Lahiri <35212670+rblcoder@users.noreply.github.com>
2024-10-28 08:05:14 +0000

    api for products and orders

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ecommerce
 Django app
+
+python manage.py makemigrations
+
+python manage.py migrate
+
+python manage.py createsuperuser
+
+python manage.py runserver
+
+urls
+
+`http://127.0.0.1:8000/api/products/` product operations.
+`http://127.0.0.1:8000/api/orders/` order operations.
---
+++ ecommerce_site/store/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+from rest_framework import viewsets
+from .models import Product, Order
+from .serializers import ProductSerializer, OrderSerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
